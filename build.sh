b12377d9ab76fce3616f54ecda92bb01363aaf8e
Mingshi <cmsax@live.com>
2019-04-16 12:04:57 +0800

    remove comment, add debug scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 (envsubst < template.yml) >> _config.yml
 
+cat _config.yml
+
 npm i -g hexo hexo-generator-search
 hexo init pages && cd pages
 npm install
 
 git clone $theme_repo themes/$theme_name
+
+ls ./themes
+
 cp -r ../articles/* ./source/_posts
 cp -f ../../.config.yml ./_config.yml
+
+cat ./_config.yml
 
 hexo generate
 
