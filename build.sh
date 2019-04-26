dc6b9746e6b00ec687b6ce3b4c9247542d390be2
蔡明师 <i@unoiou.com>
2019-04-26 22:35:36 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!bin/sh
 # Theme
-export theme_repo="https://github.com/probberechts/hexo-theme-cactus.git"
+#export theme_repo="https://github.com/probberechts/hexo-theme-cactus.git"
+export theme_repo="https://github.com/cmsax/hexo-theme-cactus.git"
 export theme_name="cactus"
 # Meta
 export hexo_title="Articles"
